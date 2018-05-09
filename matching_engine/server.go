c0df0f8a536be01ca521308e4aff5006fdbd7d3c
Wei Sun <swyddz1989@gmail.com>
2018-05-08 17:31:19 -0700

    RPC server

--- matching_engine/server.go
+++ matching_engine/server.go
@@ -1,10 +1,44 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/spaceenter/exchange/matching_engine/matcher"
+	mpb "github.com/spaceenter/exchange/matching_engine/matcher/proto"
+	"github.com/spaceenter/exchange/matching_engine/service"
+	spb "github.com/spaceenter/exchange/matching_engine/service/proto"
+	"github.com/spaceenter/exchange/store"
+	"google.golang.org/grpc"
+)
 
 var (
-	port = flag.String("port", ":50051", "Port.")
+	dataSourceName = flag.String("data_source_name", "", "Data source name.")
+	port           = flag.String("port", ":50051", "Port.")
+	tradingPair    = flag.String("trading_pair", "BTC_USD", "Trading pair.")
 )
 
 func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("net.Listen() = %v", err)
+	}
+
+	tp := mpb.TradingPair(mpb.TradingPair_value[*tradingPair])
+	matcher := matcher.New(tp)
+
+	store, err := store.New(*dataSourceName)
+	if err != nil {
+		log.Fatalf("store.New() = %v", err)
+	}
+
+	srv := service.New(matcher, store)
+
+	grpcServer := grpc.NewServer()
+	spb.RegisterMatcherServiceServer(grpcServer, srv)
+	grpcServer.Serve(lis)
 }
