15445304f1dcdd887108d85d76d4529bf9818efa
Wei Sun <swyddz1989@gmail.com>
2018-05-09 19:27:51 -0700

    dep ensure for mux; add store instance to web service.

--- web_service/rest/rest.go
+++ web_service/rest/rest.go
@@ -1,14 +1,19 @@
 package rest
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/catortiger/exchange/store"
+)
 
 type Interface interface {
 	CreateUser(w http.ResponseWriter, r *http.Request)
 }
 
 type WebService struct {
+	store *store.Store
 }
 
-func New() *WebService {
-	return &WebService{}
+func New(store *store.Store) *WebService {
+	return &WebService{store: store}
 }
--- web_service/rest_server.go
+++ web_service/rest_server.go
@@ -5,17 +5,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/catortiger/exchange/store"
 	"github.com/catortiger/exchange/web_service/rest"
 	"github.com/gorilla/mux"
 )
 
 var (
-	port = flag.String("port", ":8081", "Port.")
+	port           = flag.String("port", ":8081", "Port.")
+	dataSourceName = flag.String("data_source_name", "", "Data source name.")
 )
 
 func main() {
-	ws := rest.New()
+	store, err := store.New(*dataSourceName)
+	if err != nil {
+		log.Fatalf("store.New() = %v", err)
+	}
+
+	ws := rest.New(store)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/user", ws.CreateUser).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(*port, router))
 }
